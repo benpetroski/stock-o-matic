0c676892d8677691a2247668b39ca4830802b039
Devon Jedamski <devon.jedamski@gmail.com>
2015-11-03 22:13:25 -0800

    Major overhaul. Uses BeautifulSoup and parallel processing now to make the requests. Fully functioning release.

--- src/finviz_scrape/get_finviz_ticker_symbols.py
+++ src/finviz_scrape/get_finviz_ticker_symbols.py
@@ -1,52 +1,50 @@
-import urllib
+import requests
+from BeautifulSoup import BeautifulSoup
+from multiprocessing import Pool
 
 # Downloads the ticker symbols from finviz and prints the results to file (ticker_symbols.dat). This must be run before
 # get_finviz_data.py.
 
 
-# Number of stock can be found at finviz.com/screen.ashx
-num_stocks = 7163
+def get_script(pgnum):
+    # Download the ticker, and parse using beautiful soup
+    params = urllib.urlencode({'v': 111, 'r': pgnum})
+    html = requests.get("http://finviz.com/screener.ashx?%s" % params)
+    data = BeautifulSoup(html.content)
 
+    # Get the main table and remove the first header row
+    table = data.find('table', {'bgcolor': '#d3d3d3'})
+    rows = table.findAll('tr')
+    rows.pop(0)
+
+    # Extract the second column from the table
+    stock = []
+    for row in rows:
+        # Extracts the columns of each row
+        cols = row.findAll('td')
+        link = cols[1].findAll('a')
+        stock.append(str(link[0].text))
+
+    return stock
+
+
+# Download an example page to parse the total number of stocks
+html = requests.get("http://finviz.com/screener.ashx")
+data = BeautifulSoup(html.content)
+num = str(data.findAll('td', {'class': 'count-text', 'width': '140'})[0].text)
+num_stocks = int(num[6:-3])
 
 # Generate array of url ids (download in increments of 20)
-nums = range(1, num_stocks, 20)
+nums = range(1, num_stocks+1, 20)
 total_files = len(nums)
 
+# Start 20 processes, as they empty, a new case is fed in
+pool = Pool(processes=20)
+tickers = pool.map(get_script, nums)
 
-# Print stock names to file
+# Write the stocks to file
 fout = open('ticker_symbols.dat', 'w')
-file_count = 1
-
-
-# Loop through screen pages and parse for stock tickers
-for num in nums:
-    params = urllib.urlencode({'v': 111, 'r': num})
-    f = urllib.urlopen("http://finviz.com/screener.ashx?%s" % params)
-
-    # Open downloaded file and parse out the stock names
-    data = f.read().replace('\n', '')
-
-    # Loop through character by character and look for string
-    first = True
-    quote = ''
-    for ind, char in enumerate(data):
-        if data[ind:ind+22] == '<a href="quote.ashx?t=':
-            string = data[ind+22:ind+30]
-            amp = string.index('&')
-
-            if string[0:amp] != quote:
-                quote = string[0:amp]
-                fout.write(quote + '\n')
-                print quote
-
-            if first:
-                first_quote = quote
-                first = False
-
-    # Print out status
-    print "File " + str(file_count) + " of " + str(total_files) + " parsed (" + first_quote + " to " + quote + ")"
-    file_count += 1
-
-
-# Clean up
+for chunk in tickers:
+    for ticker in chunk:
+        fout.write('%s\n' % ticker)
 fout.close()
