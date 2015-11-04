f2e8142069938f3fde224c194bfe8a975b3714ba
Devon Jedamski <devon.jedamski@gmail.com>
2015-11-03 23:30:46 -0800

    Created the FinvizTicker class. Manages all web scraping. Temporary version of get_finviz_data. Chris fell free to revert back to what you had.

---
+++ src/finviz_scrape/FinvizTicker.py
@@ -0,0 +1,60 @@
+import requests
+from BeautifulSoup import BeautifulSoup
+from datetime import datetime
+from dateutil.tz import tzlocal
+
+class FinvizTicker:
+
+    def __init__(self, symbol):
+        # Convert the string to uppercase
+        self.symbol = symbol.upper()
+
+        # Construct the url from the ticker
+        self.url = 'http://finviz.com/quote.ashx?t=' + self.symbol
+
+        # Download the ticker, and parse using beautiful soup
+        html = requests.get(self.url)
+        self._data = BeautifulSoup(html.content)
+        self.time_stamp = datetime.now(tzlocal())
+
+        # Check if the page exists
+        if 'We cover only stocks and ETFs listed on NYSE, NASDAQ, and AMEX. International and OTC/PK are not available.' in html.content:
+            raise ImportError('Stock symbol \'' + self.symbol + '\' does not exist in the Finviz database.')
+
+        # Parse the html and create the metrics dictionary
+        self.metrics = self._get_metrics()
+
+
+    def _get_metrics(self):
+        # Extract the main table with ticker data and create a list of the rows in the table
+        table = self._data.find('table', {'class': 'snapshot-table2'})
+        rows = table.findAll('tr')
+
+        # Loop through the rows and build a dictionary of the elements
+        metrics = {}
+        for tr in rows:
+
+            # Extracts the columns of each row
+            cols = tr.findAll('td')
+
+            # Check if there is an even number of columns (should always be)
+            if len(cols) % 2 == 0:
+
+                # Extract out the unicode and convert to a raw string
+                data = [ col.text for col in cols ]
+                keys = [ str(key) for key in data[0::2] ]
+                values = [ str(value) for value in data[1::2] ]
+
+                # Add the row in to the metric database
+                metrics.update(zip(keys, values))
+            else:
+                raise ImportError('Dude, the finviz table doesn''t have an even number of columns!')
+
+        return metrics
+
+    def __str__(self):
+        return '%s: $%s\nretrieved on %s' % (self.symbol, self.metrics['Price'], ticker.time_stamp.strftime('%Y-%m-%d %H:%M:%S %Z'))
+
+
+ticker = FinvizTicker('AA')
+print ticker
--- src/finviz_scrape/get_finviz_data.py
+++ src/finviz_scrape/get_finviz_data.py
@@ -6,71 +6,15 @@ import re
 import time
 from HTMLParser import HTMLParser
 from math import ceil
+from FinvizTicker import FinvizTicker
 import sys
+from multiprocessing import Pool
 
 
-class MLStripper(HTMLParser):
-    def __init__(self):
-        self.reset()
-        self.fed = []
-
-    def handle_data(self, d):
-        self.fed.append(d)
-
-    def get_data(self):
-        return ''.join(self.fed)
-
-def strip_tags(html):
-    s = MLStripper()
-    s.feed(html)
-    return s.get_data()
-
-def scrapeticker(tickername, url):
-    try:
-        data = urllib2.urlopen(url).read()
-    except urllib2.HTTPError:
-        print "Ticker " + tickername + " could not be found because\ndevon hasn't gotten his act together to fix his ticker script! Moving to next ticker...\n"
-        return 
-    stripped_data = strip_tags(data)  # strip html tags for raw text
-
-    # Contains all relevant metrics from the table
-
-    # metricNames = ['P/E', 'EPS (ttm)', 'P/C', 'Cash/sh', 'P/C', 'EPS Q/Q', 'LT Debt/Eq', 'Debt/Eq', 'Sales Q/Q',
-    #                   'P/FCF', 'P/C', 'Index', 'Insider Own', 'Shs Outstand', 'Perf Week', 'Perf Year',
-    #                   'Market Cap', 'EPS next Y', 'Insider Trans', 'Shs Float', 'Perf Month', 'Income', 'PEG',
-    #                   'EPS next Q', 'Inst Own', 'Short Float', 'Perf Quarter', 'Sales', 'EPS this Y', 'Inst Trans',
-    #                   'Short Ratio', 'Perf Half Y', 'ROA', 'Target Price', '52W Range', 'Perf YTD', 'Dividend',
-    #                   'EPS past 5Y', 'ROI', '52W High', 'Beta', 'Quick Ratio', 'Sales past 5Y', 'Gross Margin',
-    #                   '52W Low', 'ATR', 'Employees', 'Current Ratio', 'Oper. Margin', 'Volatility', 'Optionable',
-    #                   'Profit Margin', 'Rel Volume', 'Prev Close', 'Shortable', 'Earnings', 'Payout', 'Avg Volume',
-    #                   'Price', 'Recom', 'SMA20', 'SMA50', 'SMA200', 'Volume', 'Change']
-
-    # Loop through metrics, parse, and write dict to file
-    data_dict = {}
-    for metricName in metricNames:
-        # search for the name of that metric
-        metricName = metricName.strip() # remove the newline since it was read in from a file
-        if metricName == 'Sales Q/Q1': 
-            metricName = 'Sales Q/Q' # i don't know why but for some reason we cant keep the "1" in this metric
-        search_string = metricName.replace('/', '\/').replace('(', '\(').replace(')', '\)') + '(.*)'
-        m = re.search(search_string, stripped_data)
-
-        # add the first match of the metric string; also replace metric name with nothing
-        data_dict[metricName] = m.group(0).replace(metricName, '')
-
-    # cant believe we didn't include the stockname itself in the stock's dictionary!!!
-    data_dict['stockname'] = tickername
-
-    if 'Oper. Margin' in data_dict.keys(): # fix for the "." in the "Oper. Margin" key name - can't have that if we use mongoDB - should fix in writing script!!!!
-        data_dict['Oper Margin'] = data_dict.pop('Oper. Margin')
-
-    # write the raw data into its file
-    with open('data/' + tickername + '.dat', 'w') as f:
-        #print "writing" + tickername + " url is: " + url
-        f.write(str(data_dict))
-    f.close()
-
-    data_dict.clear()  # flush the entire contents of the dictionary
+def scrapeticker(tickername):
+    stock = FinvizTicker(tickername)
+    print tickername
+    return stock.metrics
 
 
 # Read in ticker symbol list created by get_finviz_ticker_symbols.py
@@ -81,48 +25,13 @@ with open('ticker_symbols.dat') as f:
     for i, ticker in enumerate(tickers):
         tickers[i] = ticker.replace('\n', '')
 
-
-# global stuff we only have to do once
-# Read in metric names
-with open('metrics.dat') as f:
-    metricNames = f.readlines()        
-
-# Loop through tickers and scrape html
-batchsize = 1000 # i'm not sure how much we can max this out - a number on the web said "start at 100 and go from there"
+# Start 50 processes, as they empty, a new case is fed in
 tic = time.clock()
-for i in range(0,len(tickers),batchsize):
+pool = Pool(processes=100)
+metrics = pool.map(scrapeticker, tickers)
 
-    # prepare this batch of thread variables
-    tickerstrs = []
-    urls = []
-
-    if abs(len(tickers) - i+batchsize) < batchsize or i+batchsize > len(tickers): # this means we are on the last stocks... (the "remainder" if you will that won't fit in the batch number pattern) need to go from i to the len(tickers)
-        start = i
-        end = len(tickers)
-        for k in range(start,end):
-            tickerstrs.append(tickers[k])
-            urls.append('http://finviz.com/quote.ashx?t=' + tickers[k])
-
-        # designate thread targets    
-        specialrange = end-start
-        threads = [threading.Thread(target=scrapeticker, args=(tickerstrs[j], urls[j])) for j in range(specialrange)]
-
-    else:
-        start = i
-        end = i+batchsize
-        for k in range(start,end):
-            tickerstrs.append(tickers[k])
-            urls.append('http://finviz.com/quote.ashx?t=' + tickers[k])
-
-        # designate thread targets    
-        threads = [threading.Thread(target=scrapeticker, args=(tickerstrs[j], urls[j])) for j in range(batchsize)]
-
-    # start and join threads!
-    for thread in threads:
-        thread.start()
-    for thread in threads:
-        thread.join()
-
-    time_remaining = ceil((len(tickers) - i + 1)*(time.clock() - tic)/(i + 1))
-    print 'Completed tickers ' + tickers[start] + ' through ' + tickers[end-1] + ' (stocks ' + str(start) + ' through ' + str(end) + ' of ' + str(len(tickers)) + ', ' + str(time_remaining) + 's remaining)'
-
+# Write the raw data into its file
+for i, metric in enumerate(metrics):
+    with open('data/' + tickers[i] + '.dat', 'w') as fout:
+        fout.write(str(metric))
+    fout.close()
