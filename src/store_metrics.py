7074143d10cd08bae478b0e638bfeedbb8ae1d10
Chris <frewin.christopher@gmail.com>
2015-11-04 23:35:12 -0500

    moved all scripts except yahoo into main src folder
    this should allow for easier sh'ing, cron'ing

--- src/store_metrics.py
+++ src/store_metrics.py
@@ -1,3 +1,5 @@
+import subprocess
+import time
 import pymongo
 from pymongo import Connection
 import json
@@ -5,17 +7,17 @@ import glob
 import datetime
 import ntpath
 
-# this script loops through all the .dat files for every stock and puts them in a tasty mongoDB database
+# this script loops through all the .dat files for every stock and puts them in the tasty raspberry pi mongoDB database
 
-# establish mongoDB connection
-connection = Connection()
+# Our python client to the localhost mongodb
+connection = Connection('localhost', 6789)
 
 # today's timestamp will determine this particular db name's
 today = str(datetime.date.today())
 
 db = connection['stockdata']
 
-directory = '/Users/chris/github/stock-o-matic/src/finviz_scrape/data/*.dat'
+directory = '/Users/chris/github/stock-o-matic/src/data/*.dat'
 
 print "Beginning backup..."
 jsonFiles = glob.glob(directory)
@@ -25,6 +27,6 @@ for file in jsonFiles:
     stockname = stockname.split('.')[0] # get rid of the .dat ending
     data = datafile.read()
     mydict = eval(data) # eval the data (string type) to allow conversion to a python dictionary
-    db[today].insert(mydict) # insert dict as a document in the mongoDB collection for today - pymongo handles the conversion to json by itself
+    db["2015-11-03"].insert(mydict) # insert dict as a document in the mongoDB collection for today - pymongo handles the conversion to json by itself
 
 print "Done."
