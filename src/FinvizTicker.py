9eab48ebcb2ab9f1e93cbd53b06b0104ada8c2fd
Chris Frewin <frewin.christopher@gmail.com>
2021-02-08 17:57:49 +0100

    ah, much better

--- src/FinvizTicker.py
+++ src/FinvizTicker.py
@@ -1,17 +1,40 @@
 #!/usr/bin/python
 
 import requests
+import json
 from bs4 import BeautifulSoup
 from datetime import datetime
 from pytz import timezone
 
 eastern = timezone('US/Eastern')
 
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+indexes = ['DJIA', 'S&P500', 'AMEX', 'NASDAQ', 'NYSE', 'S&P']
+lastUpdatedFormat = '%Y-%m-%d'
+# lastUpdatedFormat = '%Y-%m-%d %H:%M:%S %Z'
+lastUpdatedKeyName = 'LastUpdated'
+
 class FinvizTicker:
 
     def __init__(self, ticker):
         # Convert the string to uppercase
         self.ticker = ticker.upper()
+
+        print("Constructing " + self.ticker + "...")
+
+        # Current timestamp
+        self.time_stamp = datetime.now(eastern).strftime(lastUpdatedFormat)
+
+        # First check if we've already written this in the past day
+        with open('data/tickers/' + self.ticker + '.json', 'r') as json_file:
+            data = json.load(json_file)
+            try:
+                lastUpdated = data[lastUpdatedKeyName]
+                if lastUpdated == self.time_stamp:
+                    print("We've already got this ticker's data for the day! Try again tomorrow :)")
+                    return
+            except:
+                print("No 'lastUpdated' key found, gonna go ahead and scrape this ticker!")
 
         # Construct the url from the ticker
         self.url = 'http://finviz.com/quote.ashx?t=' + self.ticker
@@ -20,7 +43,6 @@ class FinvizTicker:
         headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36"}
         response = requests.get(self.url, headers=headers)
         self._data = BeautifulSoup(response.content, 'html.parser')
-        self.time_stamp = datetime.now(eastern)
 
         # Check if the page exists - note b for bytes like object
         if b'We cover only stocks and ETFs listed on NYSE, NASDAQ, and AMEX. International and OTC/PK are not available.' in response.content:
@@ -29,8 +51,11 @@ class FinvizTicker:
         # Parse the html and create the metrics dictionary
         self.metrics = self._get_metrics()
 
+        # Write metrics to json
+        self.write_metrics()
 
     def _get_metrics(self):
+        print('Internal for ' + self.ticker)
         # Extract the main table with ticker data and create a list of the rows in the table
         table = self._data.find('table', {'class': 'snapshot-table2'})
         rows = table.findAll('tr')
@@ -44,22 +69,125 @@ class FinvizTicker:
 
             # Check if there is an even number of columns (should always be)
             if len(cols) % 2 == 0:
-
+                keys = []
+                values = []
                 # Extract out the unicode and convert to a raw string
                 data = [ col.text for col in cols ]
-                keys = [ str(key) for key in data[0::2] ]
-                values = [ str(value) for value in data[1::2] ]
 
-                # Add the row in to the metric database
+                # for each key, clean up name so it is JSON compatible
+                for key in data[0::2]:
+                    key = str(key)
+                    key = key.replace('%', 'Percent')
+                    key = key.replace('/', '')
+                    key = key.replace('(', '')
+                    key = key.replace(')', '')
+                    key = key.replace('.', '')
+                    key = key.replace('52', 'FiftyTwo')
+                    key = key.replace(' ', '')
+                    keys.append(key)
+
+                # for each value, clean up value 
+                for value in data[1::2]:
+                    value = str(value)
+                    # empty values
+                    if value == '-':
+                        values.append('')
+                        continue
+                    # for the rare triple dash!
+                    if value == '- - -':
+                        values.append('')
+                        continue
+                    # append index just as string
+                    found = False
+                    for index in indexes:
+                        if index in value:
+                            found = True
+                    
+                    if found:
+                        values.append(value)
+                        continue
+
+                    # remove percents and commas
+                    value = value.replace('%', '')
+                    value = value.replace(',', '')
+
+                    # shortable / optionable 'Yes'
+                    if value == "Yes":
+                        values.append(True)
+                        continue
+                    # shortable / optionable 'No'
+                    if value == "No":
+                        values.append(False)
+                        continue
+                    # earnings string has month and day, just append this value as string
+                    if value[0:3] in months:
+                        values.append(value)
+                        continue
+                    # 52 week range has this dash, split to array and convert to float
+                    if ' - ' in value:
+                        rangeValues = value.split(' - ')
+                        # can't do much here, just leave empty value
+                        if rangeValues[0] == '-':
+                            values.append('')
+                            continue
+                        # sometimes it only has one vol value, can only append the first
+                        if rangeValues[1] == '-':
+                            values.append([float(rangeValues[0])])
+                            continue
+                        # otherwise append the array of both
+                        values.append([float(rangeValues[0]), float(rangeValues[1])])
+                        continue
+                    # volatility has two values, split to array and convert to float
+                    if value.count(' ') == 1:
+                        volatilityVals = value.split(' ')
+                        # can't do much here, just leave empty value
+                        if volatilityVals[0] == '-':
+                            values.append('')
+                            continue
+                        # sometimes it only has one vol value, can only append the first
+                        if volatilityVals[1] == '-':
+                            values.append([float(volatilityVals[0])])
+                            continue
+                        # otherwise append the array of both
+                        values.append([float(volatilityVals[0]), float(volatilityVals[1])])
+                        continue
+                    # convert to thousands
+                    if value[-1] == 'K':
+                        value = value.replace('K', '')
+                        value = float(value)*1000
+                        values.append(value)
+                        continue
+                    # convert to millions
+                    if value[-1] == 'M':
+                        value = value.replace('M', '')
+                        value = float(value)*1000000
+                        values.append(value)
+                        continue
+                    # convert to billions
+                    if value[-1] == 'B':
+                        value = value.replace('B', '')
+                        value = float(value)*1000000000
+                        values.append(value)
+                        continue
+                    # default
+                    values.append(value)
+
+                # Custom metric: last updated so we can skip this ticker if the whole process crashes
+                keys.append(lastUpdatedKeyName)
+                values.append(self.time_stamp)
+
+                # Add the key value pair to the metric list
                 metrics.update(zip(keys, values))
             else:
                 raise ImportError('Dude, the finviz table doesn''t have an even number of columns!')
 
         return metrics
     
-    # metrics getter
-    def getMetrics(self):
-        return self.metrics
+    # metrics writer
+    def write_metrics(self):
+        with open('data/tickers/' + self.ticker + '.json', 'w') as fp:
+            json.dump(self.metrics, fp)
 
+    # string representation
     def __str__(self):
-        return '%s: $%s\nretrieved on %s' % (self.ticker, self.metrics['Price'], self.time_stamp.strftime('%Y-%m-%d %H:%M:%S %Z'))
+        return '%s: $%s\nretrieved on %s' % (self.ticker, self.metrics['Price'], self.time_stamp)
--- src/get_finviz_data.py
+++ src/get_finviz_data.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python
 
-import json
 from FinvizTicker import FinvizTicker
 from multiprocessing import Pool
 
-def scrapeticker(tickername):
-    stock = FinvizTicker(tickername)
-    print(tickername)
-    return stock.metrics
+def scrape_ticker(tickername):
+    FinvizTicker(tickername)
 
 if __name__ == '__main__':
 
     # Read in ticker symbol list created by get_finviz_ticker_symbols.py
-    with open('ticker_symbols.dat') as f:
+    with open('data/ticker_symbols.dat') as f:
         tickers = f.readlines()
 
         # Clear ticker symbols of new line character
@@ -21,10 +18,5 @@ if __name__ == '__main__':
 
     # Start processes, as they empty, a new case is fed in
     # Processes may need to be tuned to prevent being rate limited by finviz
-    pool = Pool(processes=10)
-    metrics = pool.map(scrapeticker, tickers)
-
-    # Write the metric data into it's respective ticker file in JSON format
-    for i, metric in enumerate(metrics):
-        with open('data/tickers/' + tickers[i] + '.json', 'w') as fp:
-            json.dump(metric, fp)
+    pool = Pool(processes=20)
+    pool.map(scrape_ticker, tickers)
